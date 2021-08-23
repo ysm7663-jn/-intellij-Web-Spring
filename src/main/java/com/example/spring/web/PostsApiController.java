c2df8c348e9e0abc7279921ea08904d7876bc640
ysm7663-jn <ysm7663@gmail.com>
2021-08-23 17:55:50 +0900

    PostsApiController update 추가

--- src/main/java/com/example/spring/web/PostsApiController.java
+++ src/main/java/com/example/spring/web/PostsApiController.java
@@ -2,10 +2,9 @@ package com.example.spring.web;
 
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsSaveRequestDto;
+import com.example.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 // @RequiredArgsConstructor : final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성해준다.
@@ -15,9 +14,14 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")   // 등록
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}") // 수정
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 
 }
