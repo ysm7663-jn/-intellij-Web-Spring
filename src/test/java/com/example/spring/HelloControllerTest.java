ca09618c0f71116215a507a5b3ac991d225d5b8f
ysm7663-jn <ysm7663@gmail.com>
2021-08-23 11:40:03 +0900

    HelloControllerTest HelloController 테스트

---
+++ src/test/java/com/example/spring/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.spring;
+
+import com.example.spring.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+// JUnit에 내장된 실행자 외의 다른 실행자를 실행 >> SpringRunner
+@WebMvcTest(controllers = HelloController.class)
+// Web에 집중시키는 Annotation
+// @Controller, ControllerAdvice 사용 가능
+// @Service, @Component, @Repository 사용 불가
+public class HelloControllerTest {
+
+    @Autowired
+    // @Autowired : 스프링이 관리하는 Bean을 주입 받는다
+    private MockMvc mvc;
+    // 웹 API를 테스트 할 때 사용
+    // 스프링 MVC 테스트의 시작점
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk()) // mvc.perform의 결과를 검증, HTTP Header의 Status를 검증 >> isOk == 200인지 확인
+                .andExpect(content().string(hello)); // 응답 본문 내용을 검증
+    }
+}
