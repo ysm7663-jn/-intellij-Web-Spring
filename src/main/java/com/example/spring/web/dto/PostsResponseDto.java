90e82819c4f04b91d4c9c619eeb1aa98d01e7113
ysm7663-jn <ysm7663@gmail.com>
2021-08-23 17:55:20 +0900

    PostsResponseDto PostsResponse Dto 생성

---
+++ src/main/java/com/example/spring/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.spring.web.dto;
+
+import com.example.spring.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+    // Entity의 필드 중 일부만 사용하므로 생성자로 Entity를 받아 필드에 값을 넣는다
+}
