973604f382a927c195bb972f2de3ab15d428edc1
ysm7663-jn <ysm7663@gmail.com>
2021-08-25 19:51:36 +0900

    update controller 생성

--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.example.spring.web;
 
 import com.example.spring.service.posts.PostsService;
+import com.example.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +23,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
