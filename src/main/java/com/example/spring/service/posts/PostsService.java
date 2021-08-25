48d7f2c229bacbe6a8a56a09bba208aec6b00ea2
ysm7663-jn <ysm7663@gmail.com>
2021-08-25 21:03:13 +0900

    delete 추가

--- src/main/java/com/example/spring/service/posts/PostsService.java
+++ src/main/java/com/example/spring/service/posts/PostsService.java
@@ -54,4 +54,15 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+        // JpaRepository 에서 이미 delete 메소드를 지원하고 있음
+        // 엔티티를 파라미터로 삭제할 수도 있고, deleteById 메소드를 이용하면 id 로 삭제할 수 있음
+        // 존재하는 Posts 인지 확인을 위해 엔티티 조회 후 그대로 삭제
+    }
+
 }
