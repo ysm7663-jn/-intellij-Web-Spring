f4d177a483caf093492f42dff8513fac92d9247c
ysm7663-jn <ysm7663@gmail.com>
2021-08-25 16:00:12 +0900

    PostsRepository 메인 페이지 @Query 작성

--- src/main/java/com/example/spring/domain/posts/PostsRepository.java
+++ src/main/java/com/example/spring/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.example.spring.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // @Repository   필요 없음
 public interface PostsRepository extends JpaRepository<Posts, Long> {
@@ -11,4 +14,11 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     // 주의사항!
     // Entity 클래스와 기본 Entity Repository는 함께 위치해야 함 >> 도메인 패키지에서 함께 관리k
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+    // SpringDataJpa 에서 제공하지 않는 메소드는 위 처럼 작성
+
+    // 등록/수정/삭제 >> SpringDataJpa 사용 권장
+    // 조회 >> Querydsl 사용 권장
 }
