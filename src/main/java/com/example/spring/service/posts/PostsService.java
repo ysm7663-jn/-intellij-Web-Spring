91605fc7cf4b488650c289dea6191acc3e469bda
ysm7663-jn <ysm7663@gmail.com>
2021-08-25 15:59:43 +0900

    PostsService service 새 게시글 등록 작성

--- src/main/java/com/example/spring/service/posts/PostsService.java
+++ src/main/java/com/example/spring/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.example.spring.service.posts;
 
 import com.example.spring.domain.posts.Posts;
 import com.example.spring.domain.posts.PostsRepository;
+import com.example.spring.web.dto.PostsListResponseDto;
 import com.example.spring.web.dto.PostsResponseDto;
 import com.example.spring.web.dto.PostsSaveRequestDto;
 import com.example.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,6 +40,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    // readOnly : 트랜잭션 범위는 유지, 조회 기능만 가능 >> 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                // = .map(posts -> new PostsListResponseDto(posts))
+                // postsRepository 결과로 넘어온 Posts 의 Stream 을
+               // map 을 통해 PostsListResponseDto 변환 -> List 로 반환하는 메소드
+                .collect(Collectors.toList());
     }
 
 }
