8784fdb8b8eb238ed532540b2b429e69853a60bd
ysm7663-jn <ysm7663@gmail.com>
2021-08-30 19:51:10 +0900

    Dto 기능

---
+++ src/main/java/com/example/spring/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,73 @@
+package com.example.spring.config.auth.dto;
+
+import com.example.spring.domain.user.Role;
+import com.example.spring.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    // Dto로 본다
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributes, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributes, attributes);
+    }
+
+
+    // of() : OAuth2User 에서 반환하는 사용자 정보는 Map 이기 때문에 값 하나하나를 변환해야만 한다
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    /*
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+*/
+
+    // toEntity() :
+    // User 엔티티를 생성, OAuthAttributes 에서 엔티티를 생성하는 시점은 처음 가입할 때
+    // 가입할 때 기본 권한을 GUEST 로 주기 위해서 role 빌더값에는 Role.GUEST 를 사용한다.
+    // OAuthAttribute 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+
+    }
+
+}
