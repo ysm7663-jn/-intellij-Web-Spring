502c6abf4a6b31249f5b699942d2869a92b2672a
ysm7663-jn <ysm7663@gmail.com>
2021-08-30 19:50:56 +0900

    소셜 로그인 설정

--- src/main/java/com/example/spring/config/auth/SecurityConfig.java
+++ src/main/java/com/example/spring/config/auth/SecurityConfig.java
@@ -2,15 +2,19 @@ package com.example.spring.config.auth;
 
 import com.example.spring.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity  // Spring Security 설정들을 활성화시켜 준다.
-public class SecurityConfig extends WebSecurityConfigurerAdaoter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 소셜 로그인 설정
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
-    protected void configure(HttpSecurity Http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
