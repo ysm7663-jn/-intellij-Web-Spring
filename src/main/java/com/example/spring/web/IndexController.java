9b03b5f110134db0d548f2b203efd9f88ba5eb08
ysm7663-jn <ysm7663@gmail.com>
2021-08-30 20:06:48 +0900

    index 에 SessionUser 정보를 model에 실음

--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.spring.web;
 
+import com.example.spring.config.auth.dto.SessionUser;
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 구성
+        // 로그인 성공 시 httpSession.getAttribute("user") 에서 값을 가져올 수 있음
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+            // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록
+            // 세션에 저장된 값이 없을 경우, model 에 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+        }
+
         return "index";
     }
 
