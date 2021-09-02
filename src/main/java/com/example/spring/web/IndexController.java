79b6b417de8b10ad739ecdda5a933287538c90ba
ysm7663-jn <ysm7663@gmail.com>
2021-09-02 15:52:11 +0900

    OAuth 삭제

--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.example.spring.web;
 
-import com.example.spring.config.auth.dto.SessionUser;
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -19,22 +18,7 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model, SessionUser user) {
-        model.addAttribute("posts", postsService.findAllDesc());
-
-        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        // @LoginUser 로 대체
-
-        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 구성
-        // 로그인 성공 시 httpSession.getAttribute("user") 에서 값을 가져올 수 있음
-
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userImg", user.getPicture());
-            // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록
-            // 세션에 저장된 값이 없을 경우, model 에 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
-        }
-
+    public String index() {
         return "index";
     }
 
