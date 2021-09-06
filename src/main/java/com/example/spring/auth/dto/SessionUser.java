f078dc1e8d0cbfdd5cfde47fbfa0f52e21109c49
ysm7663-jn <ysm7663@gmail.com>
2021-09-06 17:01:37 +0900

    인증된 사용자 정보와 그렇지 않은 사름의 정보

---
+++ src/main/java/com/example/spring/auth/dto/SessionUser.java
@@ -0,0 +1,22 @@
+package com.example.spring.auth.dto;
+
+import com.example.spring.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
+
+// SessionUser : 인증된 사용자 정보만 필요 >> 나머지 정보는 불 필요하니 필드로 선언
+
