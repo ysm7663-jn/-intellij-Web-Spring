3305f529dd119d52ac0471e9e11323284eb78e80
ysm7663-jn <ysm7663@gmail.com>
2021-09-06 17:00:40 +0900

    OAuth2 로그인 관련 설정

---
+++ src/main/java/com/example/spring/auth/CustomOAuth2UserService.java
@@ -0,0 +1,61 @@
+package com.example.spring.auth;
+
+import com.example.spring.auth.dto.OAuthAttributes;
+import com.example.spring.auth.dto.SessionUser;
+import com.example.spring.domain.user.User;
+import com.example.spring.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 1
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName(); // 2
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes()); // 3
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user)); // 4
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    // 5
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+
+}
+
+// 1 : 현재 로그인 진행 중인 서비스를 구분하는 코드, 구글 / 네이버 로그인 구분을 위해 사용
+// 2 : OAuth2 로그인 진행 시 키가 되는 필드값, Primary Key 와 같은 의미
+// 3 : OAuth2UserService 를 통해 가져온 OAuth2User 의 attribute 를 담은 클래스
+// 4 : 셰션에 사용자 정보를 저장하기 위한 Dto 클래스
+// 5 : 구글 사용자 정보가 업데이트 되었을 때를 대비하여 구현 >> 사용자의 이름, 프로필이 변경되면 User 엔티티에도 반영
