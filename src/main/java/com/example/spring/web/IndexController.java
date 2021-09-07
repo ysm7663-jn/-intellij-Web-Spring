0148b8249dcbb0306b1fcb4132e1b5b4c15bbf32
ysm7663-jn <ysm7663@gmail.com>
2021-09-07 14:58:27 +0900

    HttpSessionUser 를 지우고 @LoginUser 에노테이션을 파라미터로 사용

--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.spring.web;
 
+import com.example.spring.config.auth.LoginUser;
 import com.example.spring.config.auth.dto.SessionUser;
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsResponseDto;
@@ -9,21 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
-
+    // private final HttpSession httpSession; >> 중복 코드를 제거하기 위해 WebConfig, LoginUser, LoginUserArgumentResolver 를 생성함
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+        // @LoginUser SessionUser user : 기존에 (User) httpSession.getAttribute("usr") 로 가져오던 세션 정보 값이 개선됨
+        // 이제 어느 컨트롤러던지 @LoginUser 만 사용하면 세션 정보를 가져올 수 있게 됨
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user"); >> 중복 코드를 제거하기 위해 WebConfig, LoginUser, LoginUserArgumentResolver 를 생성함
         // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 구성 >> 로그인 성공 시 httpSession.getAttribute("user") 에서 값을 가져옴
         if (user != null) {
             model.addAttribute("userName", user.getName());
