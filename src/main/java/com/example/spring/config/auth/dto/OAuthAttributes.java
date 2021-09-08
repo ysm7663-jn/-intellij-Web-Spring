ffe8f19887f2028d3eb6dab4b6bfafa1c94e1902
ysm7663-jn <ysm7663@gmail.com>
2021-09-08 11:02:09 +0900

    naver 로그인 추가

--- src/main/java/com/example/spring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/spring/config/auth/dto/OAuthAttributes.java
@@ -26,10 +26,13 @@ public class OAuthAttributes {
 
     // 1
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    // 구글로 로그인
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
@@ -40,7 +43,7 @@ public class OAuthAttributes {
                 .build();
     }
 
-    /*
+    // 네이버로 로그인
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
@@ -52,7 +55,6 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
-    */
 
     // 2
     public User toEntity() {
